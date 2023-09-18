c047665505d1bd4454f16d0f2365cc17b0e8d3a7
Collin Drake <collinlindendrake@gmail.com>
2023-09-18 16:16:58 -0600

    Added a firing interval

--- main.py
+++ main.py
@@ -1,29 +1,58 @@
-# Example file showing a basic pygame "game loop"
+import threading
+import time
+from time import sleep
+
 import pygame
 import random
+
+import utils
 
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 running = True
 
+dt = 0
+
+player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+
+for i in range(10):
+    utils.Enemy(screen)
+
+lasers = []
+
+time_ = 0
+
 while running:
-    # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    # fill the screen with a color to wipe away anything from last frame
-    screen.fill("purple")
+    screen.fill("black")
+
+    dt = clock.tick(60) / 1000
 
     # RENDER YOUR GAME HERE
+    pygame.draw.circle(screen, "white", (player_pos.x, player_pos.y), 40)
 
-    # flip() the display to put your work on screen
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_w] or keys[pygame.K_UP]:
+        player_pos.y -= 300 * dt
+    if keys[pygame.K_s] or keys[pygame.K_DOWN]:
+        player_pos.y += 300 * dt
+    if keys[pygame.K_a] or keys[pygame.K_LEFT]:
+        player_pos.x -= 300 * dt
+    if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
+        player_pos.x += 300 * dt
+
+    if keys[pygame.K_SPACE] and time_ < time.time():
+        lasers.append(utils.Laser(screen, (player_pos.x, player_pos.y), "red", dt))
+        time_ = time.time() + 0.1
+
+    _ = list(map(lambda laser: laser.move(lasers), lasers))
+
+    # end of render
     pygame.display.flip()
 
-    clock.tick(60)  # limits FPS to 60
-
 pygame.quit()
-
-# pygame setup
--- utils.py
+++ utils.py
@@ -8,18 +8,24 @@ class Laser:
         self._pos = list(pos[:])
         self._color = color
         self._dt = dt
-        self._rect = pygame.draw.rect(self._screen, self._color, rect=(self._pos[0] - 4, self._pos[1] - 140, 8, 80))
+        self._rect = pygame.draw.rect(
+            self._screen,
+            self._color,
+            rect=(self._pos[0] - 4, self._pos[1] - 80, 8, 20),
+        )
 
     def move(self, lasers: list):
-        self._pos[1] -= 300 * self._dt
-
-        print(self._pos[1])
+        self._pos[1] -= 600 * self._dt
 
         if self._pos[1] < 0:
             lasers.remove(self)
         else:
             self._rect.move(0, self._pos[1])
-            pygame.draw.rect(self._screen, self._color, rect=(self._pos[0] - 4, self._pos[1] - 140, 8, 80))
+            pygame.draw.rect(
+                self._screen,
+                self._color,
+                rect=(self._pos[0] - 4, self._pos[1] - 80, 8, 20),
+            )
 
 
 class Enemy:
@@ -30,11 +36,14 @@ class Enemy:
         self._color = tuple(map(lambda x: random.randrange(0, 255), range(3)))
 
         pygame.draw.rect(
-            self._screen, self._color, rect=(
+            self._screen,
+            self._color,
+            rect=(
                 random.randrange(20, 1260),
                 random.randrange(20, 200),
                 self._box.width,
-                self._box.height)
+                self._box.height,
+            ),
         )
 
 
